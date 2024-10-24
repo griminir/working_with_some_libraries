d6e2e9a164fc240eff2b48993035076a9fcf2c52
griminir <viktor.elliot.king@gmail.com>
2024-10-24 13:08:34 +0200

    refactored

--- app_currency_converter.py
+++ app_currency_converter.py
@@ -1,37 +1,41 @@
-while True:
-    try:
-        amount = float(input('Enter the amount: '))
-        if amount <= 0:
-            raise ValueError()
-        break
-    except ValueError:
-        print('invalid amount')
+def get_amount():
+    while True:
+        try:
+            amount = float(input('Enter the amount: '))
+            if amount <= 0:
+                raise ValueError()
+            return amount
+        except ValueError:
+            print('invalid amount')
 
-currencies = ('NOK', 'USD', 'EUR')
+def get_currency(label):
+    currencies = ('NOK', 'USD', 'EUR')
+    while True:
+        currency = input(f'{label} currency (NOK/USD/EUR): ').upper()
+        if currency not in currencies:
+            print('invalid currency')
+        else:
+            return currency
+        
+def convert(amount, source_currency, target_currency):
+    # as of 24.okt 2024
+    exhange_rate = {
+        'NOK': {'USD': 0.092, 'EUR': 0.085},
+        'USD': {'NOK': 10.92, 'EUR': 0.93},
+        'EUR': {'NOK': 11.79, 'USD': 1.08}
+    }
 
-while True:
-    source_currency = input('convert from (NOK/USD/EUR): ').upper()
-    if source_currency not in currencies:
-        print('invalid currency')
-    else:
-        break
+    if source_currency == target_currency:
+        return amount
+    return amount * exhange_rate[source_currency][target_currency]
 
-while True:
-    target_currency = input('convert to (NOK/USD/EUR): ').upper()
-    if target_currency not in currencies:
-        print('invalid currency')
-    else:
-        break
+def main():
+    amount = get_amount()
+    source_currency = get_currency('Source')
+    target_currency = get_currency('Target')
+    converted_amount = convert(amount, source_currency, target_currency)
+    print(f'{amount} {source_currency} = {converted_amount:.2f} {target_currency}')
 
-# as of 24.okt 2024
-exhange_rate = {
-    'NOK': {'USD': 0.092, 'EUR': 0.085},
-    'USD': {'NOK': 10.92, 'EUR': 0.93},
-    'EUR': {'NOK': 11.79, 'USD': 1.08}
-}
-
-if source_currency == target_currency:
-    converted_amount = amount
-else:
-    converted_amount = amount * exhange_rate[source_currency][target_currency]
-print(f'{amount} {source_currency} = {converted_amount:.2f} {target_currency}')
+# this stops it from being executed if the file is imported to reuse some of the functions
+if __name__ == '__main__':
+    main()
