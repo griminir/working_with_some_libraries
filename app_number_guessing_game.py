619750023401f3a91fc40b1211d065f6e49da71d
griminir <viktor.elliot.king@gmail.com>
2024-10-24 10:39:03 +0200

    quick numbers game

---
+++ app_number_guessing_game.py
@@ -0,0 +1,29 @@
+import random
+# generate a random number
+# loop
+#   ask the user to make a guess
+#   if not a valid number
+#       print an error
+#   if number < guess
+#        print too low
+#   if number > guess
+#       print too high
+#   else
+#       print well done
+#       end loop
+
+right_number = random.randint(1, 100)
+
+while True:
+    try:
+        guess = int(input('what do you think the number is between (1-100)?: '))
+    except ValueError:
+        print('please enter a valid number')
+    else:
+        if guess < right_number:
+            print('too low')
+        elif guess > right_number:
+            print('too high')
+        else:
+            print(f'well done correct number was {right_number}')
+            break
