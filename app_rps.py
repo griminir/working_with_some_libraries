a24fa8b1a43d49e8d133a56c9c9d30d56f62ff31
griminir <viktor.elliot.king@gmail.com>
2024-10-24 11:27:33 +0200

    reafactored it into functions: modularized the code

--- app_rps.py
+++ app_rps.py
@@ -5,27 +5,42 @@ import random
 choices = ('r', 'p', 's')
 emojis = {'r': '🪨', 's': '✂️', 'p': '📃'}
 
-while True:
-    user_choice = input('Rock, paper or scissors? (r/p/s): ').lower()
-    if user_choice not in choices:
-        print('please pick r, p or s')
-        continue
 
-    computer_choice = random.choice(choices)
+def get_user_choice():
+    while True:
+        user_choice = input('Rock, paper or scissors? (r/p/s): ').lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print('please pick r, p or s')
 
+def display_choices(user_choice, computer_choice):
     print(f'you chose {emojis[user_choice]}')
     print(f'computer chose {emojis[computer_choice]}')
 
+def determin_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print('tie!')
     elif (
         (user_choice == 'r' and computer_choice == 's') or
         (user_choice == 's' and computer_choice == 'p') or
-        (user_choice == 'p' and computer_choice == 'r')):
+            (user_choice == 'p' and computer_choice == 'r')):
         print('You win!')
     else:
         print('You lose!')
 
-    should_continue = input('do you wanna continue? (y/n): ').lower()
-    if should_continue == 'n':
-        break
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+
+        display_choices(user_choice, computer_choice)
+
+        determin_winner(user_choice, computer_choice)
+
+        should_continue = input('do you wanna continue? (y/n): ').lower()
+        if should_continue == 'n':
+            break
+
+play_game()
